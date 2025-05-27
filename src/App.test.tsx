7ee763b131cbd29a0dc9f49ff0199a39cc84709d
Sam Mcloughlin <smcloughlin235@gmail.com>
2025-05-27 10:34:52 +0100

    Got rid of some files and fixed testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 // src/App.test.tsx
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
 test('renders login page or default fallback content', () => {
